d7b2d65ea5884c5c5fc83088acc4b3a623a7f49f
sako590829 <sako590829@yahoo.co.jp>
2023-04-18 05:20:28 +0900

    My Django Girls app, first commit

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
